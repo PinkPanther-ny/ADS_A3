fee33b9ad72e20f575c63ca44d37da48055d4e21
NUOYANC <nuoyanc@student.unimelb.edu.au>
2020-10-19 19:10:07 +1100

    ini

---
+++ cmake-build-debug/CMakeFiles/A3_clion.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/EVERYTHING/2020_s2/COMP20003/A3/peg_solitaire.c" "D:/EVERYTHING/2020_s2/COMP20003/A3/cmake-build-debug/CMakeFiles/A3_clion.dir/peg_solitaire.c.obj"
+  "D:/EVERYTHING/2020_s2/COMP20003/A3/src/ai.c" "D:/EVERYTHING/2020_s2/COMP20003/A3/cmake-build-debug/CMakeFiles/A3_clion.dir/src/ai.c.obj"
+  "D:/EVERYTHING/2020_s2/COMP20003/A3/src/hashtable.c" "D:/EVERYTHING/2020_s2/COMP20003/A3/cmake-build-debug/CMakeFiles/A3_clion.dir/src/hashtable.c.obj"
+  "D:/EVERYTHING/2020_s2/COMP20003/A3/src/stack.c" "D:/EVERYTHING/2020_s2/COMP20003/A3/cmake-build-debug/CMakeFiles/A3_clion.dir/src/stack.c.obj"
+  "D:/EVERYTHING/2020_s2/COMP20003/A3/src/utils.c" "D:/EVERYTHING/2020_s2/COMP20003/A3/cmake-build-debug/CMakeFiles/A3_clion.dir/src/utils.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/A3_clion.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "A3_clion.exe"
+  "A3_clion.exe.manifest"
+  "A3_clion.lib"
+  "A3_clion.pdb"
+  "CMakeFiles/A3_clion.dir/peg_solitaire.c.obj"
+  "CMakeFiles/A3_clion.dir/src/ai.c.obj"
+  "CMakeFiles/A3_clion.dir/src/hashtable.c.obj"
+  "CMakeFiles/A3_clion.dir/src/stack.c.obj"
+  "CMakeFiles/A3_clion.dir/src/utils.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/A3_clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
